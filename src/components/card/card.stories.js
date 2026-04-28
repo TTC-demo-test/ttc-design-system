f07dec2f4147e95cf9d03b895ba5e0c4ec943218
Calvin Webster <calvin.webster@tridenttech.edu>
2026-04-28 11:29:23 -0400

    adding sample card to trigger storybook gh-pages deployment

--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -32,6 +32,23 @@ export const SunsetCard = () => `
   </div>
 `
 
+export const SunriseCard = () => `
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://plus.unsplash.com/premium_photo-1676320526001-07b75bd19ae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZXxlbnwwfHwwfHx8MA%3D%3D" alt="Sunset in the mountains">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Warmest Sunrise</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    </div>
+  </div>
+  `
+
 export const PhotoGridx2 = () => `
   <div class="mx-auto my-4 max-w-6xl px-4">
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
