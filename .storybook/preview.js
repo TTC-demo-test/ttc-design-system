9c7489ba2d3cf5716b4a7bf42c7b18d0205b7800
Calvin Webster <calvin.webster@tridenttech.edu>
2026-04-29 10:52:57 -0400

    added some storybook addons, a11y and vitest, also a small component accessibility messages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
 import '../src/styles/app.css'
 
 export const parameters = {
-    layout: 'centered'
+    layout: 'centered',
+
+    a11y: {
+        // 'todo' - show a11y violations in the test UI only
+        // 'error' - fail CI on a11y violations
+        // 'off' - skip a11y checks entirely
+        test: 'todo'
+    }
 }
