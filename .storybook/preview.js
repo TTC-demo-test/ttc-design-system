8dfd197e2a3060400fd9d15f247df626330031cc
Calvin Webster <calvin.webster@tridenttech.edu>
2026-05-04 09:54:55 -0400

    added a bunch of card examples from flowbite site

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,12 @@ export default definePreview({
 
 export const parameters = {
     layout: 'centered',
-
+    docs: {
+        codePanel: true,
+        source: {
+            transform: (code) => code.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+        }
+    },
     a11y: {
         // 'todo' - show a11y violations in the test UI only
         // 'error' - fail CI on a11y violations
